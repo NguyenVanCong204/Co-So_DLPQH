571b36eacca51fb5b0b8f6c58b5b082bc1255fc9
Nguyen Van Cong <congnguyenvan522@gmail.com>
2025-12-17 23:08:58 +0700

    delete blog

--- src/index.js
+++ src/index.js
@@ -1,38 +1,40 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 const app = express();
-import mongoose from "mongoose";
-import apiRouter from "./router/Admin/api.js";
-import apiRouterMember from "./router/Member/api.js";
-import authRouter from "./router/Auth/auth.js";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import apiRouter from './router/Admin/api.js';
+import apiRouterMember from './router/Member/api.js';
+import authRouter from './router/Auth/auth.js';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT;
 
-app.use("/public", express.static("public"));
+app.use('/public', express.static('public'));
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+);
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/admin", apiRouter);
-app.use("/api/member", apiRouterMember);
-app.use("/api", authRouter);
+app.use('/api/admin', apiRouter);
+app.use('/api/member', apiRouterMember);
+app.use('/api', authRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express.js!");
+app.get('/', (req, res) => {
+    res.send('Hello, Express.js!');
 });
 
 mongoose
-  .connect(DATABASE_URL)
-  .then(() => console.log("Database is connected successfully"))
-  .catch((err) => console.log("Database connection error : ", err));
+    .connect(DATABASE_URL)
+    .then(() => console.log('Database is connected successfully'))
+    .catch((err) => console.log('Database connection error : ', err));
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
